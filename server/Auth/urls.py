f464e75c594fdb5c6d0d3b5f1637c8cbc3c7109d
kristian diez <isnomatophobic@gmail.com>
2022-10-12 01:14:50 +0300

    BE:Logout/Regiser

--- server/Auth/urls.py
+++ server/Auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Register,Is_Logged
+from .views import Register,Is_Logged,Login,Logout
 
 urlpatterns = [
     path('register',Register,name="register"),
+    path('login',Login,name="login"),
+    path('logout',Logout,name="logout"),
     path('islogged',Is_Logged,name="is logged")
 
 ]
--- server/Auth/views.py
+++ server/Auth/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse,HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth import login 
+from django.contrib.auth import login,authenticate,logout
 
 @csrf_exempt
 def Register(req):
@@ -13,6 +13,25 @@ def Register(req):
     print(form.errors)
     return HttpResponseBadRequest()
 
+@csrf_exempt
+def Login(req):
+    if req.user.is_authenticated:
+        return HttpResponseBadRequest('User already logged in')
+    username = req.POST['username']
+    password = req.POST['password']
+    user = authenticate(req, username=username, password=password)
+    if user is not None:
+        login(req, user)
+        return HttpResponse(f'User logged in as {username}')
+        # Redirect to a success page.
+    return HttpResponseBadRequest('Invalid username/password combination')
+
+@csrf_exempt
+def Logout(req):
+    if not req.user.is_authenticated:
+        return HttpResponseBadRequest('User not logged in')
+    logout(req)
+    return HttpResponse('User logged out')
 
 @csrf_exempt
 def Is_Logged(req):
