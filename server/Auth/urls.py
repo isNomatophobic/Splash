183ddbd87af9efe947a2b0da15c0a263ba7c7887
kristian diez <isnomatophobic@gmail.com>
2022-10-12 01:07:11 +0300

    Working login

--- server/Auth/urls.py
+++ server/Auth/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Register
+from .views import Register,Is_Logged
 
 urlpatterns = [
-    path('register',Register,name="register")
+    path('register',Register,name="register"),
+    path('islogged',Is_Logged,name="is logged")
+
 ]
--- server/Auth/views.py
+++ server/Auth/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse,HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth import login 
+
+@csrf_exempt
 def Register(req):
-    return HttpResponse()
+    form = UserCreationForm(req.POST) 
+    if form.is_valid():  
+        user = form.save()
+        login(req,user)
+        return HttpResponse()
+    print(form.errors)
+    return HttpResponseBadRequest()
+
+
+@csrf_exempt
+def Is_Logged(req):
+    return HttpResponse(req.user.is_authenticated)
